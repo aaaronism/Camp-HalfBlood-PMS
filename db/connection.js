b14c6bad813fb654684516b64acc93d52b6adf36
aaaronism <aaronlai01@gmail.com>
2022-04-27 20:58:16 -0400

    heroku 3

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const mongoURI = process.env.NODE_ENV === "production"
 ? process.env.DB_URL
-: "mongodb+srv://aaaronism:JccdQ2oSSM8BLdwj@camphalfblood.no61s.mongodb.net/userData?retryWrites=true&w=majority"
+: process.env.DEV_DB_URL
 
 mongoose.connect(mongoURI)
 .then(instance => console.log(`connected to ${instance.connections[0].name}`))
