14ef653ab931aaa26f38efdd5e2b0a019a42364b
aaaronism <aaronlai01@gmail.com>
2022-04-27 21:36:46 -0400

    heroku 4

--- db/connection.js
+++ db/connection.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const mongoURI = process.env.NODE_ENV === "production"
 ? process.env.DB_URL
 : process.env.DEV_DB_URL
-
+console.log(mongoURI)
 mongoose.connect(mongoURI)
 .then(instance => console.log(`connected to ${instance.connections[0].name}`))
-.catch(error => console.log('failed conn:', `${err}`))
+.catch(error => console.log('failed conn:', `${error}`))
 
 module.exports = mongoose
